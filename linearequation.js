7ccb84fc8c16b85ff59f116585aa022e9502599d
Veronika <72261304+knika-starkey@users.noreply.github.com>
2021-09-24 21:20:49 +0300

    Update linearequation.js

--- linearequation.js
+++ linearequation.js
@@ -1,6 +1,10 @@
 alert("a*x+b=0");
 let a = prompt("Enter a");
 let b = prompt("Enter b");
-alert(a+"x*+"+b+"=0");
-let x = -b/a;
-alert("x=-"+b+"/"+a+"="+x);
+if (isNaN(a) || isNaN(b)) alert("a must be number");
+else if (a == 0) alert("a should not be equal to 0");
+else {
+  alert(a + "* x +" + b + "= 0");
+  let x = -b / a;
+  alert("x=-" + b + "/" + a + "=" + x);
+}
